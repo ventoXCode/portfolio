ae7854a9910d79c77df0ab84625beab5cf881136
Joris Geis <87895936+ventoXCode@users.noreply.github.com>
2024-09-29 21:48:05 +0200

    Redownload

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,11 +1,12 @@
-import { baseURL } from "@/app/resources";
+import { baseURL } from '@/app/resources'
 
 export default function robots() {
-  return {
-    rules: [
-      {
-        userAgent: "*",
-      },
-    ],
-  };
+    return {
+        rules: [
+            {
+                userAgent: '*',
+            },
+        ],
+        sitemap: `${baseURL}/sitemap.xml`,
+    }
 }
